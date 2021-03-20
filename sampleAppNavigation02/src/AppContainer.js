ba061af73e89a4bea0ba8938190bce8a583a87ba
mshige1979 <mshige1979@ryuouen.net>
2021-03-21 01:34:59 +0900

    プラットフォームでの起動タイミングを調整

--- sampleAppNavigation02/src/AppContainer.js
+++ sampleAppNavigation02/src/AppContainer.js
@@ -1,5 +1,12 @@
-import React, {useEffect, useRef} from 'react';
-import {View, Text, Button, TouchableOpacity, AppState} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  AppState,
+  Platform,
+} from 'react-native';
 import Counter from './components/Counter';
 import FooterTabs from './navigations/FooterTabs';
 import {
@@ -10,24 +17,57 @@ import {
 const AppConteiner = () => {
   const appState = useRef(AppState.currentState);
 
+  /**
+   * 初回レンダリング
+   */
   useEffect(() => {
+    console.log('AppConteiner start');
     AppState.addEventListener('change', _handleAppStateChange);
+
+    // iosの初回起動時はなぜか動作しないため、手動で実行させる
+    if (Platform.OS === 'ios') {
+      _handleAppStateChange('active');
+    }
 
     return () => {
       AppState.removeEventListener('change', _handleAppStateChange);
     };
   }, []);
 
+  /**
+   * アプリ状態の検知
+   * @param {*} nextAppState
+   */
   const _handleAppStateChange = nextAppState => {
+    console.log('nextAppState:', nextAppState);
+
+    // 初回起動時
+    if (appState.current.match(/active/) && nextAppState === 'active') {
+      console.log('application start');
+
+      // 初期処理
+    }
+
+    // 起動後に別タスクからの復帰時
     if (
       appState.current.match(/inactive|background/) &&
       nextAppState === 'active'
     ) {
       console.log('App has come to the foreground!');
+
+      // 一部のみ初期処理
     }
 
-    console.log('AppState nextAppState:', nextAppState);
-    console.log('AppState', appState.current);
+    // backgroundへ移行時
+    if (nextAppState.match(/background/)) {
+      console.log('application background');
+
+      // なんかあったらやる
+    }
+
+    // ステータス値を更新
+    appState.current = nextAppState;
+    //console.log('AppState', appState.current);
   };
 
   return (
