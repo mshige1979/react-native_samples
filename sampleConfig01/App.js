025ecf703cbd68a11ddb8537d6a02f8fedce236f
mshige1979 <mshige1979@ryuouen.net>
2021-03-20 13:39:30 +0900

    no message

---
+++ sampleConfig01/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import Config from 'react-native-config';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>APP_NAME: {Config.APP_NAME}</Text>
+      <Text>VERSION: {Config.VERSION}</Text>
+    </View>
+  );
+};
+
+export default App;
