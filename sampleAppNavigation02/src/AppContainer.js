69f568a31810eb2c1753406740e34ab903821352
mshige1979 <mshige1979@ryuouen.net>
2021-03-21 01:08:48 +0900

    アプリの状態を監視

--- sampleAppNavigation02/src/AppContainer.js
+++ sampleAppNavigation02/src/AppContainer.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, Text, Button, TouchableOpacity} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {View, Text, Button, TouchableOpacity, AppState} from 'react-native';
 import Counter from './components/Counter';
 import FooterTabs from './navigations/FooterTabs';
 import {
@@ -8,6 +8,28 @@ import {
 } from '@react-navigation/native';
 
 const AppConteiner = () => {
+  const appState = useRef(AppState.currentState);
+
+  useEffect(() => {
+    AppState.addEventListener('change', _handleAppStateChange);
+
+    return () => {
+      AppState.removeEventListener('change', _handleAppStateChange);
+    };
+  }, []);
+
+  const _handleAppStateChange = nextAppState => {
+    if (
+      appState.current.match(/inactive|background/) &&
+      nextAppState === 'active'
+    ) {
+      console.log('App has come to the foreground!');
+    }
+
+    console.log('AppState nextAppState:', nextAppState);
+    console.log('AppState', appState.current);
+  };
+
   return (
     <NavigationContainer>
       <FooterTabs />
